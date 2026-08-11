da95273f9167bca8efc0f7bab975a524bf4535ba
Sooriya <158829004+Sooriya04@users.noreply.github.com>
2026-08-11 06:57:42 +0530

    Update redis.js

--- shared/cache/redis.js
+++ shared/cache/redis.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis');
 
-const redisClient = new Redis(process.env.REDIS_URL || 'redis://:tce_redis_secure_password@localhost:6379');
+const redisClient = new Redis(process.env.REDIS_URL);
 
 redisClient.on('connect', () => {
   console.log('Connected to Redis server.');
